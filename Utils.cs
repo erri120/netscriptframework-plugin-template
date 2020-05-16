493fd67e1964d3c3c5713d48bc0be99054535110
erri120 <erri120@protonmail.com>
2020-05-16 17:34:26 +0200

    Initial commit

---
+++ Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NetScriptFramework.Tools;
+
+namespace PluginTemplate
+{
+    public static class Utils
+    {
+        private static readonly LogFile LogFile;
+
+        static Utils()
+        {
+            LogFile = new LogFile(Plugin.PluginName, LogFileFlags.AppendFile | LogFileFlags.AutoFlush);
+        }
+
+        public static void Log(string s)
+        {
+            LogFile.AppendLine(s);
+        }
+
+        public static void Do<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable) action(item);
+        }
+    }
+}
