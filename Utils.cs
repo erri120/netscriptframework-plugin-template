c3e4049a6e1cc0b2d5410c0bf75d55ca10c1b004
erri120 <erri120@protonmail.com>
2020-05-17 14:47:56 +0200

    Updated Logging functions

--- Utils.cs
+++ Utils.cs
@@ -10,12 +10,15 @@ namespace PluginTemplate
 
         static Utils()
         {
-            LogFile = new LogFile(Plugin.PluginName, LogFileFlags.AppendFile | LogFileFlags.AutoFlush);
+            //optional to also use LogFileFlags.IncludeTimestampInFileName
+            LogFile = new LogFile(Plugin.PluginName, LogFileFlags.AppendFile | LogFileFlags.AutoFlush | LogFileFlags.IncludeTimestampInLine);
         }
 
         public static void Log(string s)
         {
             LogFile.AppendLine(s);
+            //uncomment if you want to display the log message on the HUD, useful when for debugging
+            //NetScriptFramework.SkyrimSE.MenuManager.ShowHUDMessage(s, null, true);
         }
 
         public static void Do<T>(this IEnumerable<T> enumerable, Action<T> action)
